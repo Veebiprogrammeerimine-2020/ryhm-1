313a52ad7430bae70b2b3c1ac2a413580a173a6b
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-09-08 11:09:20 +0300

    esimene üleslaadimine

---
+++ home.php
@@ -0,0 +1,32 @@
+<?php
+  $username = "Andrus Rinde";
+  $fulltimenow = date("d.m.Y H:i:s");
+  $hournow = date("H");
+  $partofday = "lihtsalt aeg";
+  if($hournow < 6){
+	  $partofday = "uneaeg";
+  }//enne 6
+  if($hournow >= 8 and $hournow <= 18){
+	  $partofday = "õppimise aeg";
+  }
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $username; ?> programmeerib veebi</title>
+
+</head>
+<body>
+  <h1><?php echo $username; ?></h1>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
+  <p><?php echo "Praegu on " .$partofday ."."; ?></p>
+</body>
+</html>
+
+
+
+
+
