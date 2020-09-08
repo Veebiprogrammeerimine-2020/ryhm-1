a9fb1814483090d152f24fd3ede3983c2dc15fa9
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-09-08 11:28:55 +0300

    Teine tund lõpetatud

--- home.php
+++ home.php
@@ -9,6 +9,15 @@
   if($hournow >= 8 and $hournow <= 18){
 	  $partofday = "õppimise aeg";
   }
+  
+  //vaatame semestri kulgemist
+  $semesterstart = new DateTime("2020-8-31");
+  $semesterend = new DateTime("2020-12-13");
+  $semesterduration = $semesterstart->diff($semesterend);
+  $semesterdurationdays = $semesterduration->format("%r%a");
+  $today = new DateTime("now");
+  
+  
 ?>
 <!DOCTYPE html>
 <html lang="et">
