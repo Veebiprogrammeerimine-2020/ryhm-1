bb71005b4ae4ea8b27f8fa80879208aec770ec91
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-09-15 09:49:24 +0300

    Kolmanda tunni esimene pool tehtud

--- tund3/home.php
+++ tund3/home.php
@@ -3,6 +3,13 @@
   $fulltimenow = date("d.m.Y H:i:s");
   $hournow = date("H");
   $partofday = "lihtsalt aeg";
+  $weekdaynameset = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+  $monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+  //echo $weekdaynameset;
+  //var_dump($weekdaynameset);
+  $weekdaynow = date("N");
+  //echo $weekdaynow;
+  
   if($hournow < 6){
 	  $partofday = "uneaeg";
   }//enne 6
@@ -29,9 +36,7 @@
   //saime aja erinevuse objektina, seda niisama näidata ei saa
   $fromsemesterstartdays = $fromsemesterstart->format("%r%a");
   $semesterpercentage = 0;
-  
-  
-  
+    
   $semesterinfo = "Semester kulgeb vastavalt akadeemilisele kalendrile.";
   if($semesterstart > $today){
 	  $semesterinfo = "Semester pole veel peale hakanud!";
@@ -50,7 +55,35 @@
 	  $semesterinfo = "Semester on läbi saanud!";
   }
 
+  //annan ette lubatud pildivormingute loendi
+  $picfiletypes = ["image/jpeg", "image/png"];
+  //loeme piltide kataloogi sisu ja näitame pilte
+  //$allfiles = scandir("../vp_pics/");
+  $allfiles = array_slice(scandir("../vp_pics/"), 2);
+  //var_dump($allfiles);
+  //$picfiles = array_slice($allfiles, 2);
+  $picfiles = [];
+  //var_dump($picfiles);
+  foreach($allfiles as $thing){
+	$fileinfo = getImagesize("../vp_pics/" .$thing);
+    //var_dump($fileinfo);
+	if(in_array($fileinfo["mime"], $picfiletypes) == true){
+		array_push($picfiles, $thing);
+	}
+  }
   
+  
+  //paneme kõik pildid ekraanile
+  $piccount = count($picfiles);
+  //$i = $i + 1;
+  //$i ++;
+  //$i += 2;
+  $imghtml = "";
+  //<img src="../vp_pics/failinimi.png" alt="tekst">
+  for($i = 0; $i < $piccount; $i ++){
+	  $imghtml .= '<img src="../vp_pics/' .$picfiles[$i] .'" ';
+	  $imghtml .= 'alt="Tallinna Ülikool">';
+  }
   
 ?>
 <!DOCTYPE html>
@@ -61,12 +94,15 @@
 
 </head>
 <body>
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
   <h1><?php echo $username; ?></h1>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
-  <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
+  <p>Lehe avamise hetk: <?php echo $weekdaynameset[$weekdaynow - 1] .", " .$fulltimenow; ?>.</p>
   <p><?php echo "Praegu on " .$partofday ."."; ?></p>
   <p><?php echo $semesterinfo; ?></p>
+  <hr>
+  <?php echo $imghtml; ?>
 </body>
 </html>
 
