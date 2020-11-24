8649727c35ad3434207f99c3f808ce743d74a1dc
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-11-24 09:51:09 +0200

    12 tund, uudiste toimetaja lisatud

---
+++ tund12/addnews.php
@@ -0,0 +1,58 @@
+<?php
+  require("usesession.php");
+  require("../../../../config_vp2020.php");
+  //require("fnc_photo.php");
+  require("fnc_common.php");
+  //require("classes/Photoupload_class.php");
+  
+  $tolink = "\t" .'<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>' ."\n";
+  $tolink .= "\t" .'<script>tinymce.init({selector:"textarea#newsinput", plugins: "link", menubar: "edit",});</script>' ."\n";
+    
+  $inputerror = "";
+  $notice = null;
+  $news = null;
+  
+    
+  //kui klikiti submit, siis ...
+  if(isset($_POST["newssubmit"])){
+	
+  }
+  
+  require("header.php");
+?>
+  <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?></h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  
+  <ul>
+    <li><a href="?logout=1">Logi välja</a>!</li>
+    <li><a href="home.php">Avaleht</a></li>
+  </ul>
+  
+  <hr>
+  
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
+    <label for="newstitleinput">Sisesta uudise pealkiri</label>
+	<input id="newstitleinput" name="newstitleinput" type="text" required>
+	<br>
+	<label for="newsinput">Kirjuta uudis</label>
+	<textarea id="newsinput" name="newsinput"><?php echo $news; ?></textarea>
+	
+	<br>
+		
+	<input type="submit" id="newssubmit" name="newssubmit" value="Salvesta uudis">
+  </form>
+  <p id="notice">
+  <?php
+	echo $inputerror;
+	echo $notice;
+  ?>
+  </p>
+  
+</body>
+</html>
+
+
+
+
+
--- tund12/home.php
+++ tund12/home.php
@@ -40,6 +40,7 @@
 	<li><a href="userprofile.php">Minu kasutajaprofiil</a></li>
 	<li><a href="photoupload.php">Galeriipiltide üleslaadimine</a></li>
 	<li><a href="photogallery_public.php">Avalike fotode galerii</a></li>
+	<li><a href="addnews.php">Uudise lisamine</a></li>
   </ul>
   
   <hr>
